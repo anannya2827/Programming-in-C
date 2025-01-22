d1b1aa62fed6f6c4699a31705fa3e79072d1122d
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-01-22 16:06:55 +0530

    Delete chardemo.c

--- chardemo.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-  char value;
-  printf("Enter a character:");
-  value=getchar();
-  //scanf("%c",&value); used to get char but there are many limitations
-  printf("%c",value);
-}
