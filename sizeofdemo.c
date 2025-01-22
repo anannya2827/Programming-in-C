31a9f467f7939f2920e90fb8ba4484571aae4b09
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-01-22 16:21:55 +0530

    Add files via upload

---
+++ sizeofdemo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  printf("Size of Integer=%ld",sizeof(int));
+  printf("\nSize of Long Integer=%ld",sizeof(long int));
+  printf("\nSize of Short Integer=%ld",sizeof(short int));
+  printf("\nSize of Float=%ld",sizeof(float));
+  printf("\nSize of Double=%ld",sizeof(double));
+  printf("\nSize of char=%ld",sizeof(char));
+  return 0;
+}
