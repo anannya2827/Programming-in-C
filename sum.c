3690393f7614695a843d271b6164cad2bd495460
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-01-22 15:25:59 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -2,9 +2,9 @@
 int main(){
   int num1,num2,sum;
   printf("\nEnter two integers numbers:");
-  scanf("%d%d",&num1,&num2);
+  scanf("%d%d",&num1,&num2);//'scanf'used to get value from user;"%d" used to define type of data(here int);'&'used to indicate to the address of the variable specified
   sum=num1+num2;
-  printf("Sum=%d\n",sum);
+  printf("Sum=%d\n",sum);//'printf'used to print value;'Sum=%d\n' '%d' specifies datatype;\n for next command to be in new line 
   printf("%d+%d=%d\n",num1,num2,sum);
   return 0;
 }   
