3c49a194a63fbd61ab32e3e3c8905080e6c8b11a
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-01-22 14:57:45 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,sum;
+  printf("\nEnter two integers numbers:");
+  scanf("%d%d",&num1,&num2);
+  sum=num1+num2;
+  printf("Sum=%d\n",sum);
+  printf("%d+%d=%d\n",num1,num2,sum);
+  return 0;
+}   
