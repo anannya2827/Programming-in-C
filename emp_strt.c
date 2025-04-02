c1e9c72418b28380c98a35dc4751b11b59f2d1f3
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-04-02 16:02:22 +0530

    Add files via upload

---
+++ emp_strt.c
@@ -0,0 +1,32 @@
+/* Name:Anannya
+Date:2/4/25
+Structure with employee details using typedef  */
+#include<stdio.h>
+typedef struct{
+   char name[20];
+   int id;
+   float salary;
+}emp;
+int main(){
+   int n;
+   printf("Enter number of employees:");
+   scanf("%d",&n);
+   emp s[n];
+   for(int i=0;i<n;i++){
+      printf("Enter the name:");
+      scanf("%s",s[i].name);
+      printf("Enter the id:");
+      scanf("%d",&s[i].id);
+      printf("Enter the salary:");
+      scanf("%f",&s[i].salary);
+      printf("\n");
+   }
+   printf("The details are:");
+   for(int i=0;i<n;i++){
+      printf("Name:%s",s[i].name);
+      printf("\nRoll no:%d",s[i].id);
+      printf("\nSalary:%f\n",s[i].salary);
+      printf("\n");
+   }
+return 0;
+}
