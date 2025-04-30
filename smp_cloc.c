90718612313fa8d263e6ba1ae40ce10f7b607edb
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-04-30 14:45:48 +0530

    Update smp_cloc.c

--- smp_cloc.c
+++ smp_cloc.c
@@ -6,12 +6,12 @@ int main(){
    printf("\n Enter the no of elements:");
    scanf("%d",&n);
    array=(int *)calloc(n,sizeof(int));
-   printf("\n Enter the elements");
+   printf("\n Enter the elements\n");
    for(int i=0;i<n;i++){
       scanf("%d",array+i);
    }
    for(int i=0;i<n;i++){
-      printf("%d",*(array+i));
+      printf("%d\t",*(array+i));
    }
    return 0;
 }
