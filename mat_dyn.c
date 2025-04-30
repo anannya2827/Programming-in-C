3f688699118c76e6d5397f3eb1e5d73e94c4ef51
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-04-30 15:52:55 +0530

    Add files via upload

---
+++ mat_dyn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   int **matrix;
+   int rows=2,cols=3;
+   matrix=(int *)malloc(rows*sizeof(int *));
+   for(int i=0;i<rows;i++){
+      matrix[i]=(int*)malloc(cols*sizeof(int*));
+   }
+   printf("\nEnter the elements");
+   for(int i=0;i<rows;i++){
+      for(int j=0;j<cols;j++){
+           scanf("%d",(*(matrix+i)+j));
+       }
+   }
+   return 0;
+}
